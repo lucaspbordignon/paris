d216d4d2bda60206a514563dfeaed046cb4145f0
Lucas Bordignon <lucaspbordignon99@gmail.com>
2017-01-12 12:48:54 -0200

    Disk partition added

--- init.sh
+++ init.sh
@@ -18,11 +18,15 @@ Menu() {
     done
 }
 
+Read_choice() {
+    read ANSW
+    ANSW=$( echo $ANSW | tr "[:upper:]" "[:lower:]" ) # Convert to lower case
+}
+
 # Set keyboard layout
 Keyboard() {
     echo "List layouts? [y/n]"
-    read ANSW
-    ANSW=$( echo $ANSW | tr "[:upper:]" "[:lower:]" ) # Convert to lower case
+    Read_choice
     if [ $ANSW = y ]; then
         less ls /usr/share/kbd/keymaps/**/*.map.gz
     fi
@@ -31,21 +35,37 @@ Keyboard() {
     loadkeys ANSW
 }
 
-# Connects to wifi and test the connection
+# Connects to wifi and tests the connection
 Wifi() {
     ANSW=n
     while [ $ANSW != y ]; do
         wifi-menu
         echo "Done? [y/n]"
-        read ANSW
+        Read_choice
     done
     echo "Test internet? [y/n]"
-    read ANSW
+    Read_choice
     if [ $ANSW = y ]; then
         ping -c3 google.com
     fi
 }
 
+Disk() {
+    FINISHED=n
+    while [ $FINISHED != y ]; do
+        echo "List devices? [y/n]"
+        Read_choice
+        if [ $ANSW = y ]; then
+            fdisk -l
+        fi
+        echo "Device to be used (/dev/sda, etc.):"
+        read ANSW
+        fdisk ANSW
+        echo "Done? [y/n]"
+        Read_choice
+        FINISHED=ANSW
+    done 
+}
 # Init
 echo "Welcome to PARIS (Personal Arch linux Installation Script)."
 Menu
@@ -53,8 +73,8 @@ case $CHOICE in
     0) return ;;
     1) Keyboard;;
     2) Wifi;;
-    3);;
-    4);;
+    3) timedatectl set-ntp true;;
+    4) Disk;;
     5);;
     6);;
     7);;
