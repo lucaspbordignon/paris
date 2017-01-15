d7359dd8f88c111feab60a4fd927269f8ec2d9a9
Lucas Bordignon <lucaspbordignon99@gmail.com>
2017-01-15 17:47:29 -0200

    Style improved

--- second.sh
+++ second.sh
@@ -1,23 +1,43 @@
 #!/bin/sh
-echo "Setting the timezone"
+
+# Useful
+Read_choice() {
+    read ANSW
+    ANSW=$( echo $ANSW | tr "[:upper:]" "[:lower:]" ) # Convert to lower case
+}
+
+echo "Installing script dependencies."
+pacman -S vim
+
+echo -e "\nSetting the timezone and setting hw clock."
 ln -s /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
-hweclock --systohc
+hwclock --systohc
 vim /etc/locale.gen
 echo "Generating locale, for language support"
 locale-gen
-echo "Creating a host name"
+
+echo -e "\nCreating a host name"
 vim /etc/hostname
-wifi-menu
+
+echo -e "\nInstalling wifi related packages and "
 pacman -S iw wpa_supplicant dialog
-mkinitcpio -p linux
+
+echo -e "\nCreating root password."
 passwd
+echo "Create a new user?[y/n]"
+Read_choice
+if [ $ANSW = y ]; then
+    echo "User name:"
+    read ANSW
+    useradd -m -G wheel -s /bin/bash $ANSW
+    passwd $ANSW
+fi
+
 # Bootloader
-echo "Installing bootloader (grub)"
+echo -e "\nInstalling bootloader (grub)"
 pacman -S grub efibootmgr
 echo "Device to install: "
-Read_choice
+read ANSW
 grub-install $ANSW
 grub-mkconfig -o /boot/grub/grub.cfg
-echo "Defining root passwd."
-passwd
 # Desktop Environment
